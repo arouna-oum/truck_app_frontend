ec049f35dd293b53ff2c5e28bd40f81f9169bab8
Oumarou Arouna <oumarouarouna65@email.com>
2026-05-20 04:48:47 +0100

    first commit2

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "leaflet/dist/leaflet.css";
-import Header from './components/header'
-import Footer from './components/Footer'
+import Header from './components/header';
+import Footer from './components/Footer';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
